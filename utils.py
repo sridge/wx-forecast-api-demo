893db931ba5b6533bc404a5461ff3df584040b87
sridge <smr1020@gmail.com>
2020-07-21 18:00:23 -0400

    update plotting function

---
+++ utils.py
@@ -0,0 +1,44 @@
+import bokeh
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, output_file, show
+
+def plot_forecast(x,json_output,quantity,units,color):
+    
+    h = bokeh.models.tools.HoverTool(
+        tooltips=[
+            ('temperature', '$y{0.1f}°C'),
+            ( 'date',   '@x{%I %p %F}'            ),
+        ],
+
+        formatters={
+            '@x'        : 'datetime', # use 'datetime' formatter for '@date' field
+                                         # use default 'numeral' formatter for other fields
+        },
+
+        # display a tooltip whenever the cursor is vertically in line with a glyph
+        mode='vline'
+    )
+    
+    s = bokeh.models.tools.PanTool()
+
+    fig = figure(x_axis_type='datetime', title=f'{quantity} ({units})', x_range=(x[1], x[120]),
+                 tools = [h,s],
+                 height=300,width=600, toolbar_location="below",background_fill_alpha=0)
+
+
+    fig.varea(x=x[1:],
+              y1 = json_output['forecast'][quantity.lower()]['min_poss'][1:],
+              y2 = json_output['forecast'][quantity.lower()]['max_poss'][1:],
+              fill_color='#D1D1D1',
+              alpha=0.5
+             )
+
+    fig.line(x=x[1:],y=json_output['forecast'][quantity.lower()]['best_guess'][1:],
+             line_color=color,line_width=3,)
+
+    fig.xgrid.grid_line_color = None
+    fig.ygrid.grid_line_width = 0.5
+
+
+
+    show(fig)
